5f7523f2239fcaeb20dfa2b9f4594257a97cdb7f
Luke Stagner <lstagner@uci.edu>
2020-02-23 15:54:44 -0800

    impurities and charge numbers

--- src/constants.jl
+++ src/constants.jl
@@ -1,3 +1,5 @@
+# Source CODATA2018 https://physics.nist.gov/cuu/Constants/
+
 # Re-export units
 @reexport using Unitful: m, cm, s, kg, g, eV, keV, c, u, b, C, J, A, Hz, rad, T, V, Ω, W, K, Pa, angstrom, Å, cal, atm, Torr, mol, lm
 const MeV = u"MeV"
@@ -16,10 +18,7 @@ uconvert(a::T, x::S) where {T<:TemperatureUnits, S<:Energy} = uconvert(a, uconve
 # import constants defined in Unitful
 import Unitful: c0, μ0, k, q, ϵ0, ε0, Z0, me, mp, mn, G, gn, h, ħ, μB, Na, R, σ, R∞, μB
 
-# MFE Formulary http://library.psfc.mit.edu/catalog/online_pubs/MFE_formulary_2014.pdf
-# Values from CODATA
-
-# Masses
+# Masses and Charges
 const AtomicMassUnit = uconvert(kg, 1u)
 const mu = AtomicMassUnit
 export AtomicMassUnit
@@ -29,21 +28,27 @@ const ElectronAtomicMass = 5.48579909065e-4u
 const Ae = ElectronAtomicMass
 const ElectronRestMass = 0.51099895000MeV/c^2
 const me0 = ElectronRestMass
-export ElectronMass, ElectronAtomicMass, ElectronRestMass
+const ElectronChargeNumber = -1
+const Ze = ElectronChargeNumber
+export ElectronMass, ElectronAtomicMass, ElectronRestMass, ElectronChargeNumber
 
 const ProtonMass = mp
 const ProtonAtomicMass = 1.007276466621u
 const Ap = ProtonAtomicMass
 const ProtonRestMass = 938.27208816MeV/c^2
 const mp0 = ProtonRestMass
-export ProtonMass, ProtonAtomicMass, ProtonRestMass
+const ProtonChargeNumber = 1
+const Zp = ProtonChargeNumber
+export ProtonMass, ProtonAtomicMass, ProtonRestMass, ProtonChargeNumber
 
 const NeutronMass = mn
 const NeutronAtomicMass = 1.00866491595u
 const An = NeutronAtomicMass
 const NeutronRestMass = 939.56542052MeV/c^2
 const mn0 = NeutronRestMass
-export NeutronMass, NeutronAtomicMass, NeutronRestMass
+const NeutronChargeNumber = 0
+const Zn = NeutronChargeNumber
+export NeutronMass, NeutronAtomicMass, NeutronRestMass, NeutronChargeNumber
 
 const DeuteronMass = 3.3435837724e-27kg
 const md = DeuteronMass
@@ -51,7 +56,9 @@ const DeuteronAtomicMass = 2.013553212745u
 const Ad = DeuteronAtomicMass
 const DeuteronRestMass = 1875.61294257MeV/c^2
 const md0 = DeuteronRestMass
-export DeuteronMass, DeuteronAtomicMass, DeuteronRestMass
+const DeuteronChargeNumber = 1
+const Zd = DeuteronChargeNumber
+export DeuteronMass, DeuteronAtomicMass, DeuteronRestMass, DeuteronChargeNumber
 
 const TritonMass = 5.0073567446e-27kg
 const mt = TritonMass
@@ -59,7 +66,9 @@ const TritonAtomicMass = 3.01550071621u
 const At = TritonAtomicMass
 const TritonRestMass = 2808.92113298MeV/c^2
 const mt0 = TritonRestMass
-export TritonMass, TritonAtomicMass, TritonRestMass
+const TritonChargeNumber = 1
+const Zt = TritonChargeNumber
+export TritonMass, TritonAtomicMass, TritonRestMass, TritonChargeNumber
 
 const HelionMass = 5.0064127796e-27kg
 const mh = HelionMass
@@ -67,7 +76,9 @@ const HelionAtomicMass = 3.014932247175u
 const Ah = HelionAtomicMass
 const HelionRestMass = 2808.39160743MeV/c^2
 const mh0 = HelionRestMass
-export HelionMass, HelionAtomicMass, HelionRestMass
+const HelionChargeNumber = 2
+const Zh = HelionChargeNumber
+export HelionMass, HelionAtomicMass, HelionRestMass, HelionChargeNumber
 
 const AlphaMass = 6.6446573357e-27kg
 const mα = AlphaMass
@@ -78,8 +89,68 @@ const Aa = Aα
 const AlphaRestMass = 3727.3794066MeV/c^2
 const mα0 = AlphaRestMass
 const ma0 = mα0
-export AlphaMass, AlphaAtomicMass, AlphaRestMass
+const AlphaChargeNumber = 2
+const Zα = AlphaChargeNumber
+const Za = Zα
+export AlphaMass, AlphaAtomicMass, AlphaRestMass, AlphaChargeNumber
 
+# Impurity Atomic Masses
+const LithiumAtomicMass = 6.941u
+const Lithium6AtomicMass = 6.938u
+const Lithium7AtomicMass = 6.997u
+const LithiumMass = uconvert(kg, LithiumAtomicMass)
+const Lithium6Mass = uconvert(kg, Lithium6AtomicMass)
+const Lithium7Mass = uconvert(kg, Lithium7AtomicMass)
+const LithiumChargeNumber = 1
+export LithiumAtomicMass, Lithium6AtomicMass, Lithium7AtomicMass
+export LithiumMass, Lithium6Mass, Lithium7Mass
+export LithiumChargeNumber
+
+const BerylliumAtomicMass = 9.0121831u
+const BerylliumMass = uconvert(kg, BerylliumAtomicMass)
+const BerylliumChargeNumber = 2
+export BerylliumAtomicMass, BerylliumMass, BerylliumChargeNumber
+
+const BoronAtomicMass = 10.811u
+const Boron10AtomicMass = 10.806u
+const Boron11AtomicMass = 10.821u
+const BoronMass = uconvert(kg, BoronAtomicMass)
+const Boron10Mass = uconvert(kg, Boron10AtomicMass)
+const Boron11Mass = uconvert(kg, Boron11AtomicMass)
+const BoronChargeNumber = 5
+export BoronAtomicMass, Boron10AtomicMass, Boron11AtomicMass
+export BoronMass, Boron10Mass, Boron11Mass
+export BoronChargeNumber
+
+const CarbonAtomicMass = 12.011u
+const Carbon12AtomicMass = 12.0096u
+const Carbon13AtomicMass = 12.0116u
+const CarbonMass = uconvert(kg, CarbonAtomicMass)
+const Carbon12Mass = uconvert(kg, Carbon12AtomicMass)
+const Carbon13Mass = uconvert(kg, Carbon13AtomicMass)
+const CarbonChargeNumber = 6
+export CarbonAtomicMass, Carbon12AtomicMass, Carbon13AtomicMass
+export CarbonMass, Carbon12Mass, Carbon13Mass
+export CarbonChargeNumber
+
+const NitrogenAtomicMass = 14.007u
+const Nitrogen14AtomicMass = 14.00643u
+const Nitrogen15AtomicMass = 14.00728u
+const NitrogenMass = uconvert(kg, NitrogenAtomicMass)
+const Nitrogen14Mass = uconvert(kg, Nitrogen14AtomicMass)
+const Nitrogen15Mass = uconvert(kg, Nitrogen15AtomicMass)
+const NitrogenChargeNumber = 7
+export NitrogenAtomicMass, Nitrogen14AtomicMass, Nitrogen15AtomicMass
+export NitrogenMass, Nitrogen14Mass, Nitrogen15Mass
+export NitrogenChargeNumber
+
+const OxygenAtomicMass = 15.999u
+const OxygenMass = uconvert(kg, OxygenAtomicMass)
+const OxygenChargeNumber = 8
+export OxygenAtomicMass, OxygenMass, OxygenChargeNumber
+
+
+# Physical Constants
 const ProtonElectronMassRatio = 1836.15267343
 const ElectronProtonMassRatio = 5.44617021487e-4
 export ProtonElectronMassRatio, ElectronProtonMassRatio
@@ -104,7 +175,8 @@ export BolzmannConstant, StephanBoltzmannConstant
 const SpeedOfLightInVacuum = c0
 const PermittivityOfFreeSpace = ε0
 const PermeabilityOfFreeSpace = μ0
-export SpeedofLightInVacuum, PermittivityOfFreeSpace, PermeabilityOfFreeSpace
+const ImpedanceOfVacumm = Z0
+export SpeedofLightInVacuum, PermittivityOfFreeSpace, PermeabilityOfFreeSpace, ImpedanceOfVacuum
 
 const RydbergConstant = R∞
 const Ry = 13.605693122994eV
@@ -169,7 +241,7 @@ const MolarWeightOfAir = M_air
 export AvogadroNumber, FaradayConstant, MolarGasConstant, LoschmidtsNumber
 export StandardTemperature, StandardPressure, StandardVolume, MolarWeightOfAir
 
-# CODATA Wallet https://physics.nist.gov/cuu/pdf/wallet_2018.pdf
+# Miscellaneous Constants
 const Cs133HyperfineTransitionFrequency = 9192631770Hz
 const ΔνCs = Cs133HyperfineTransitionFrequency
 const LuminousEfficacy = 683lm/W
